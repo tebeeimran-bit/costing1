e0d009c883c7c8dd28d04e23b87c2d791d943199
Robi Sumantri <lambdalambada04@gmail.com>
2026-03-26 07:32:27 +0000

    feat: add document receipt module with list-first UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CostingController;
 use App\Http\Controllers\DatabaseController;
+use App\Http\Controllers\DocumentReceiptController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CostingController::class, 'dashboard'])->name('dashboard');
@@ -20,3 +21,8 @@ Route::post('/database/cycle-time-templates', [DatabaseController::class, 'store
 Route::delete('/database/cycle-time-templates/{id}', [DatabaseController::class, 'destroyCycleTimeTemplate'])->name('database.cycle-time-templates.destroy');
 Route::get('/form', [CostingController::class, 'form'])->name('form');
 Route::post('/costing/store', [CostingController::class, 'store'])->name('costing.store');
+Route::get('/document-receipts', [DocumentReceiptController::class, 'index'])->name('document-receipts.index');
+Route::post('/document-receipts', [DocumentReceiptController::class, 'store'])->name('document-receipts.store');
+Route::get('/document-receipts/{documentReceipt}/{type}', [DocumentReceiptController::class, 'download'])
+	->where('type', 'partlist|umh')
+	->name('document-receipts.download');
